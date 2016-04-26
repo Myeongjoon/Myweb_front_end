1c7a61e19632dbf06df9fa7abdc8b5018b39eb35
System Administrator <root@kimui-MacBook-Pro-2.local>
2016-04-26 15:51:21 +0900

    server.js -> get "/lol_home"

--- server.js
+++ server.js
@@ -121,6 +121,10 @@
 	app.get('/', function(req, res) {
 		res.sendfile('index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
+	
+	app.get('/lol_home', function(req, res) {
+		res.sendfile('lol_home.html'); //go to lol_home.htmls
+	});
 
 	// listen (start app with node server.js) ======================================
 	app.listen(8080, argv.fe_ip);

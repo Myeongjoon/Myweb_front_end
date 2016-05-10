e121538ee25529448a9be2f3da46bea30995d4d3
Myeongjoon <kimmj8409@gmail.com>
2016-05-10 23:13:14 +0900

    test

--- server.js
+++ server.js
@@ -153,6 +153,7 @@
 
 	var fileupload = require('fileupload').createFileUpload('//home/kimmj8409/Myweb_front_end').middleware
 	app.post('/upload', fileupload, function(req, res) {
+		console.log("/upload");
 		fs.readFile(req.files.uploadFile.path,function(error,data){
 			var filePath = ___dirname + req.files.uploadFile.name;
 			fs.writeFile(filePath,data,function(error){

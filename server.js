d7a6f77011fd3cce16df45b7babc958fb5f07c37
Myeongjoon <kimmj8409@gmail.com>
2016-05-12 18:22:26 +0900

    f

--- server.js
+++ server.js
@@ -150,7 +150,9 @@
 		console.log("/query.n3 accessed");
 	});
 	
-	app.use(bodyParser({keepExtensions:true,uploadDir:path.join(__dirname,'/files')}));
+	app.use(bodyParser.urlencoded());
+
+	app.use(bodyParser.json());
 	
 	// listen (start app with node server.js) ======================================
 	var busboy = require('connect-busboy');

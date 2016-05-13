6f81981292f997189e4158c9751bb1ed4993033f
Myeongjoon <kimmj8409@gmail.com>
2016-05-13 17:19:32 +0900

    app.post('/upload', upload.single('fileUpload'), function (req, res, next) {

--- server.js
+++ server.js
@@ -1,79 +1,44 @@
-/**
- * @license
- * Everything in this repo is MIT License unless otherwise specified.
- *
- * Copyright (c) Addy Osmani, Sindre Sorhus, Pascal Hartig, Stephen  Sawchuk, Google, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-	// set up ========================
 	var express = require('express');
 	var path = require('path');
     var logger = require('morgan');
     var methodOverride = require('method-override');
-	var app      = express(); 								// create our app w/ express
-	var mongoose = require('mongoose'); 					// mongoose for mongodb
-	var morgan = require('morgan'); 			// log requests to the console (express4)
-	var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
-	var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+	var app      = express(); 							
+	var mongoose = require('mongoose'); 			
+	var morgan = require('morgan'); 		
+	var bodyParser = require('body-parser'); 
+	var methodOverride = require('method-override');
 	var argv = require('optimist').argv;
 	var multer  = require('multer')
 	var fs = require('fs');
 	var formidable = require('formidable');
-	// configuration =================
+	
+	var upload = multer();
+	app.use(multer({ dest: './uploads/'}))
 
 	mongoose.connect('mongodb://' + argv.be_ip + ':80/my_database');
     app.use('/js', express.static(__dirname + '/js'));
    	app.use('/bower_components', express.static(__dirname + '/bower_components'));
-	app.use(morgan('dev')); 										// log every request to the console
-	//app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
-	//app.use(bodyParser.json()); 									// parse application/json
-	//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+	app.use(morgan('dev')); 									
 	app.use(methodOverride());
 
-	// define model =================
 	var Todo = mongoose.model('Todo', {
 		title : String,
 		completed: Boolean
 	});
 
-	// routes ======================================================================
-
-	// api ---------------------------------------------------------------------
-	// get all todos
 	app.get('/api/todos', function(req, res) {
 
-		// use mongoose to get all todos in the database
 		Todo.find(function(err, todos) {
 
-			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 			if (err)
 				res.send(err)
 
-			res.json(todos); // return all todos in JSON format
+			res.json(todos);
 		});
 	});
 
-	// create todo and send back all todos after creation
 	app.post('/api/todos', function(req, res) {
 
-		// create a todo, information comes from AJAX request from Angular
 		Todo.create({
 			title : req.body.title,
 			completed : false
@@ -81,7 +46,6 @@
 			if (err)
 				res.send(err);
 
-			// get and return all the todos after you create another
 			Todo.find(function(err, todos) {
 				if (err)
 					res.send(err)
@@ -104,15 +68,12 @@
 	  });
 	});
 
-	// delete a todo
 	app.delete('/api/todos/:todo_id', function(req, res) {
 		Todo.remove({
 			_id : req.params.todo_id
 		}, function(err, todo) {
 			if (err)
 				res.send(err);
-
-			// get and return all the todos after you create another
 			Todo.find(function(err, todos) {
 				if (err)
 					res.send(err)
@@ -121,52 +82,53 @@
 		});
 	});
 
-	// application -------------------------------------------------------------
 	app.get('/', function(req, res) {
-		res.sendfile('index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile('index.html');
 	});
 	
 	app.get('/lol_home', function(req, res) {
-		res.sendfile('lol_home.html'); //go to lol_home.htmls
+		res.sendfile('lol_home.html');
 	});
 	
 	app.get('/four_in_a_row', function(req, res) {
-		res.sendfile('four_in_a_row.html'); //go to lol_home.htmls
+		res.sendfile('four_in_a_row.html');
 	});
 	
 	app.get('/media/:id', function(req, res) {
-		res.sendfile('media/'+req.params.id); //go to lol_home.htmls
+		res.sendfile('media/'+req.params.id);
 	});
 	
 	app.get('/data.n3', function(req, res) {
-		res.sendfile('data.n3'); //go to lol_home.htmls
+		res.sendfile('data.n3');
 		console.log("/data.n3 accessed");
 	});
 
 	app.get('/query.n3', function(req, res) {
 		console.log("/query.n3 accessed");
-		res.sendfile('query.n3'); //go to lol_home.htmls
+		res.sendfile('query.n3');
 		console.log("/query.n3 accessed");
 	});
-	
-	app.use(bodyParser.urlencoded({ extended: true }));
+	var upload = multer({ storage:storage }).single('file')
+	//app.post('/upload', upload.single('fileUpload'), function (req, res, next) {
+	app.post('/upload', function (req, res, next) {	
+		console.log('uploading---')
+    console.log(req)
+    var storage = multer.diskStorage({
+          destination: function (req, file, cb) {
+            console.log(req.body)
+            cb(null, ('hidden/images/slip/' +req.body.classId) )
+          },
+          filename: function (req, file, cb) {
+            cb(null, file.fieldname + '-' + Date.now())
+          }
+     })
 
-	//app.use(bodyParser.json());
-	
-	// listen (start app with node server.js) ======================================
-	var upload = multer()
-	app.use(multer({ dest: './uploads/'}))
-	console.log("upload.array(\"fileUpload\")")
-	console.log(upload.array("fileUpload"))
-	console.log("upload.array()")
-	console.log(upload.array())
-	console.log("upload")
-	console.log(upload)
-	app.post('/upload', upload.single('fileUpload'), function (req, res, next) {
+    //var upload = multer({ dest: ('hidden/images/slip/' + req.body.classId) }).single('file')
+    var upload = multer({ storage:storage }).single('fileUpload')
+		
 		var form = new formidable.IncomingForm();
 		form.keepExtensions = true;
 		form.on('file', function(field, file) {
-            //On file received
             console.log(file);
         })
 		console.log("req.files : ");
@@ -174,13 +136,7 @@
 		console.log("req.body : ");
 		console.log(req.body);
 		console.log("req : "+req)
-		console.log("res : "+res)
-		console.log(res)
-		console.log("next : "+next)
 		res.send(200);
-  // req.file is the `avatar` file 
-  // req.body will hold the text fields, if there were any 
 	})
-	
 	app.listen(8080, argv.fe_ip);
 	console.log("App listening on port 8080");
